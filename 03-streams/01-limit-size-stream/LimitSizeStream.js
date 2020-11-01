7e0d3744e0d126ec657c4fb5bfc818847f3bcef7
Vsevolod Chuprygin <VChuprygin@luxoft.com>
2020-11-01 19:52:32 +0300

    LimitSizeStream
    Hometask #3

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -4,10 +4,18 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this._limit = options.limit;
+    this._passed = 0;
   }
 
   _transform(chunk, encoding, callback) {
-
+    const byteLength = chunk.byteLength;
+    this._passed += byteLength;
+    if (this._passed > this._limit) {
+      callback(new LimitExceededError());
+      return;
+    }
+    callback(null, chunk);
   }
 }
 
